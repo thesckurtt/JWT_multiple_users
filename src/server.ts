5ee92a884f082946e89f550b55fa2bffa62e5963
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2024-12-25 14:35:31 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,5 @@
 import fastify from "fastify";
-
-const app = fastify({logger: true});
-
-app.get("/", ()=>{
-  return "Hello world"
-})
+export const app = fastify({logger: true});
 
 app.listen({
   host: "0.0.0.0",
