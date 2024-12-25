29522e4a584e5c02a1062e3f4b9afe860a212d0c
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2024-12-25 16:09:21 -0300

    Update env_config.ts

--- src/Config/env_config.ts
+++ src/Config/env_config.ts
@@ -4,3 +4,5 @@ dotenv.config();
 export const env_JWT_GLOBAL_SECRET_KEY : string  = process.env.JWT_GLOBAL_SECRET_KEY ?? "";
 export const env_JWT_USERS_SECRET_KEY : string  = process.env.JWT_USERS_SECRET_KEY ?? "";
 export const env_JWT_ADMINS_SECRET_KEY : string  = process.env.JWT_ADMINS_SECRET_KEY ?? "";
+export const env_JWT_EXPIRATION_TIME : string  = process.env.JWT_EXPIRATION_TIME ?? "";
+export const env_PREFIX_AUTH_ROUTES : string  = process.env.PREFIX_AUTH_ROUTES ?? "";
