716edf1016935cf6c2d10ed70a9a50e45f7826fe
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2024-12-25 20:23:45 -0300

    Update User.ts

--- src/Database/Seeds/User.ts
+++ src/Database/Seeds/User.ts
@@ -1,10 +1,21 @@
 import { prisma } from "../../Config/db_config";
 import { Users } from "../Models/Users.js";
-export async function UserSeed(turns?: number) {
-  Users.createUser({
-    email: "",
-    name: "",
-    password: "",
-    isAdmin: false,
-  });
+import { faker } from "@faker-js/faker";
+
+export async function UserSeed(turns: number = 1) {
+  for (let i = 1; i <= turns; i++) {
+    try {
+      const start = performance.now();
+      await Users.createUser({
+        email: faker.internet.email(),
+        name: faker.person.fullName(),
+        password: "admin",
+        isAdmin: false,
+      });
+      const end = performance.now();
+      console.log(`A operação levou ${(end - start).toFixed(2)}ms`);
+    } catch (error) {
+      throw new Error(`ERROR: ${error}`);
+    }
+  }
 }
