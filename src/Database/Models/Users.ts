14b7a405fe9784f342683dccc2bac42fe47c3cc7
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2024-12-25 20:23:56 -0300

    Update Users.ts

--- src/Database/Models/Users.ts
+++ src/Database/Models/Users.ts
@@ -4,19 +4,20 @@ import argon2 from "argon2";
 
 export class Users {
   public static async createUser(data: UserInterface) {
-
-    const hashPassword : string = await argon2.hash(data.password);
-
-    const result = await prisma.user.create({
-      data: {
-        name: data.name,
-        email: data.email,
-        password: hashPassword,
-        isAdmin: data.isAdmin,
-      },
-    });
-
-    console.info(result);
+    const hashPassword: string = await argon2.hash(data.password);
+    try {
+      const result = await prisma.users.create({
+        data: {
+          name: data.name,
+          email: data.email,
+          password: hashPassword,
+          isAdmin: data.isAdmin,
+        },
+      });
+      console.info(result);
+    } catch (error) {
+      throw new Error(`Error: ${error}`)
+    }
   }
   public static async isValidUser(email: string, password: string) {}
 }
