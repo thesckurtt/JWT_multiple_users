1fb9bce7328018a5ddc85349ff452099a59c8d43
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2024-12-25 15:24:32 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,21 @@
 import fastify from "fastify";
-export const app = fastify({logger: true});
+// import { AuthRoutes } from "./Routes/AuthRoutes.js";
+import { RegisterRoute } from "./Routes/Routes.js";
+export const app = fastify({ logger: true });
 
-app.listen({
-  host: "0.0.0.0",
-  port: 3333
-}, (err, address)=>{
-  console.log(`\nServer listening at: ${address} \n`)
-})
+// Registers
+RegisterRoute(app);
+
+app.listen(
+  {
+    host: "0.0.0.0",
+    port: 3333,
+  },
+  (err: unknown, address: string) => {
+    if (err) {
+      console.error("├ ERROR ──────── \n");
+      throw new Error(`${err} \n`);
+    }
+    console.log(`\nServer listening at: ${address} \n`);
+  }
+);
