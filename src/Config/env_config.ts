fd00eea49465f65a855461f1b952339d11120dc1
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2024-12-26 20:56:54 -0300

    Update env_config.ts

--- src/Config/env_config.ts
+++ src/Config/env_config.ts
@@ -7,3 +7,4 @@ export const env_JWT_ADMINS_SECRET_KEY : string  = process.env.JWT_ADMINS_SECRET
 export const env_JWT_EXPIRATION_TIME : string  = process.env.JWT_EXPIRATION_TIME ?? "";
 export const env_PREFIX_AUTH_ROUTES : string  = process.env.PREFIX_AUTH_ROUTES ?? "";
 export const env_SALT_PASSWORD : string = process.env.SALT_PASSWORD ?? "";
+export const env_PREFIX_ADMIN_ROUTES : string = process.env.PREFIX_ADMIN_ROUTES ?? "";
