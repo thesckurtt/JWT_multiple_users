a93c06becee31ebb519913eff1820b3dc4815a0c
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2024-12-26 17:16:46 -0300

    Update AuthController.ts

--- src/Controller/AuthController/AuthController.ts
+++ src/Controller/AuthController/AuthController.ts
@@ -2,6 +2,7 @@ import { FastifyRequest, FastifyReply } from "fastify";
 import { AuthControllerLoginSchema } from "./Schemas/AuthControllerLoginSchema.js";
 import { Authenticator } from "../../Utils/Authenticator.js";
 import { Users } from "../../Database/Models/Users.js";
+import { app } from "../../server.js";
 
 export class AuthController {
   static async login(request: FastifyRequest, reply: FastifyReply) {
@@ -16,7 +17,13 @@ export class AuthController {
 
     const isValidUser = await Users.isValidUser(value.email, value.password);
 
-    console.log(isValidUser);
-    return isValidUser;
+    console.log(isValidUser?.user?.email);
+
+    if (isValidUser.isValid) {
+      if (isValidUser?.user?.isAdmin) {
+        return app.jwt.sign({ isAdmin: true }, { expiresIn: "1h" });
+      }
+      return app.jwt.sign({ isAdmin: false }, { expiresIn: "1h" });
+    }
   }
 }
