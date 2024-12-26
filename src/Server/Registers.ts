54912545c663180bc53a36c106cad394d04989ee
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2024-12-26 17:16:28 -0300

    Update Registers.ts

--- src/Server/Registers.ts
+++ src/Server/Registers.ts
@@ -1,12 +1,11 @@
-import { app } from "server.js";
-import jwtPlugin from "@fastify/jwt";
+import { app } from "../server.js";
+import { FastifyInstance } from "fastify";
+import fastifyJwt  from "@fastify/jwt";
 import { env_JWT_GLOBAL_SECRET_KEY, env_JWT_USERS_SECRET_KEY, env_JWT_ADMINS_SECRET_KEY} from "../Config/env_config.js";
 
-/**
- * ### JWT Secret Keys for use in middlewares
- */
-const JWT_SECRET_KEYS = [env_JWT_GLOBAL_SECRET_KEY, env_JWT_USERS_SECRET_KEY, env_JWT_ADMINS_SECRET_KEY];
-
-app.register(jwtPlugin, {
-  secret: JWT_SECRET_KEYS[0]
-});
+export async function Registers(fastify: FastifyInstance) {
+  // console.log(env_PREFIX_AUTH_ROUTES);
+  fastify.register(fastifyJwt,{
+    secret: env_JWT_GLOBAL_SECRET_KEY ?? "djs89dfsua8hr8qdn8aowehf9"
+  }); 
+}
