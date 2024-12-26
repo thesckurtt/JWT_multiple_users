aab64e453c1f3cc361f8b51314999e9e62533c09
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2024-12-25 21:53:02 -0300

    Delete AuthController.ts

--- src/Controller/AuthController.ts
+++
@@ -1,7 +0,0 @@
-import { FastifyRequest, FastifyReply } from "fastify";
-
-export class AuthController{
-  static async login(request: FastifyRequest, reply: FastifyReply) {
-    return "Login controller function";
-  }
-}
