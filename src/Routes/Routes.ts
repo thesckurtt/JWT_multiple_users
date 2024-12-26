5ef80952338005ed266a8e482cfc3a2251b15151
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2024-12-26 20:57:08 -0300

    Update Routes.ts

--- src/Routes/Routes.ts
+++ src/Routes/Routes.ts
@@ -1,8 +1,9 @@
 import { FastifyInstance } from "fastify";
 import { AuthRoutes } from "./AuthRoutes.js";
-import { env_PREFIX_AUTH_ROUTES } from "../Config/env_config.js";
-
+import { AdminRoutes } from "./AdminRoutes.js"
+import { env_PREFIX_AUTH_ROUTES, env_PREFIX_ADMIN_ROUTES } from "../Config/env_config.js";
 export async function RegisterRoute(fastify: FastifyInstance) {
   console.log(env_PREFIX_AUTH_ROUTES);
   fastify.register(AuthRoutes, {prefix: env_PREFIX_AUTH_ROUTES}); // Registers AuthRoutes
+  fastify.register(AdminRoutes, {prefix: env_PREFIX_ADMIN_ROUTES })
 }
