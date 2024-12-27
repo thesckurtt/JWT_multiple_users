4a5e970c4b43954d02855b39db5910a1c0148a0d
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2024-12-27 11:17:20 -0300

    Update Registers.ts

--- src/Server/Registers.ts
+++ src/Server/Registers.ts
@@ -1,18 +1,17 @@
 import { FastifyInstance } from "fastify";
-import fastifyJwt  from "@fastify/jwt";
+import fastifyJwt from "@fastify/jwt";
 import { env_JWT_GLOBAL_SECRET_KEY } from "../Config/env_config.js";
 import { AdminMiddleware } from "../Middlewares/AdminMiddleware.js";
+import { UserMiddleware } from "../Middlewares/UserMiddleware.js";
 
 export async function Registers(fastify: FastifyInstance) {
-  fastify.register(fastifyJwt,{
-    secret: env_JWT_GLOBAL_SECRET_KEY ?? "djs89dfsua8hr8qdn8aowehf9"
-  });  
+  fastify.register(fastifyJwt, {
+    secret: env_JWT_GLOBAL_SECRET_KEY ?? "djs89dfsua8hr8qdn8aowehf9",
+  });
 
-  fastify.decorate(
-    "AdminAuthenticate",
-    AdminMiddleware
-  );
+  fastify.decorate("AdminAuthenticate", AdminMiddleware);
 
-  fastify.decorate("AdminMiddleware", AdminMiddleware)
-  
+  // fastify.decorate("AdminMiddleware", AdminMiddleware);
+
+  fastify.decorate("UserAuthenticate", UserMiddleware)
 }
