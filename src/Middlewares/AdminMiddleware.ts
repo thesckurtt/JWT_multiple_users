7cd4a8dce8a5844dfcac6f31b6c60d01b6f4bae4
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2024-12-26 21:53:20 -0300

    Update AdminMiddleware.ts

--- src/Middlewares/AdminMiddleware.ts
+++ src/Middlewares/AdminMiddleware.ts
@@ -1,8 +1,19 @@
+import { error } from "console";
 import { FastifyRequest, FastifyReply } from "fastify";
 
 export async function AdminMiddleware(
   request: FastifyRequest,
   reply: FastifyReply
 ) {
-  console.log("Middleware Admin");
+  try{
+    // { isAdmin: false, iat: 1735259201, exp: 1735262801 }
+    const jwt : {isAdmin?: boolean, iat?: number|string, exp?: number|string} = await request.jwtVerify();
+
+    if(!jwt.isAdmin){
+      throw new Error();
+    }
+    // console.log(jwt.isAdmin)
+  }catch(error){
+    reply.code(401).send({ error: "Unauthorized" });
+  }
 }
