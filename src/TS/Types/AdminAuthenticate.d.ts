81a89088d51a3ade259d3540f891e0c2a0704c80
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2024-12-27 11:17:22 -0300

    Update AdminAuthenticate.d.ts

--- src/TS/Types/AdminAuthenticate.d.ts
+++ src/TS/Types/AdminAuthenticate.d.ts
@@ -3,5 +3,6 @@ import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
 declare module "fastify" {
   interface FastifyInstance {
     AdminAuthenticate: (request: FastifyRequest, reply: FastifyReply) => Promise<void>;
+    UserAuthenticate: (request: FastifyRequest, reply: FastifyReply) => Promise<void>;
   }
 }
