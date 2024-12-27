78a441699f77545a372e7d1acebe12c076414654
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2024-12-26 23:12:07 -0300

    Update AdminMiddleware.ts

--- src/Middlewares/AdminMiddleware.ts
+++ src/Middlewares/AdminMiddleware.ts
@@ -1,18 +1,23 @@
+import { prisma } from "../Config/db_config.js"
 import { error } from "console";
+import { BlackListJWT } from "../Database/Models/BlackListJWT.js";
 import { FastifyRequest, FastifyReply } from "fastify";
+import { object } from "joi/lib/index.js";
 
 export async function AdminMiddleware(
   request: FastifyRequest,
   reply: FastifyReply
 ) {
   try{
-    // { isAdmin: false, iat: 1735259201, exp: 1735262801 }
     const jwt : {isAdmin?: boolean, iat?: number|string, exp?: number|string} = await request.jwtVerify();
 
-    if(!jwt.isAdmin){
+    const isBlockedJwt = await BlackListJWT.isInBlackList((request.headers.authorization)?.replace("Bearer ","") ?? "");
+
+    if(!jwt.isAdmin || isBlockedJwt){
       throw new Error();
     }
-    // console.log(jwt.isAdmin)
+
+    // console.log(jwt)
   }catch(error){
     reply.code(401).send({ error: "Unauthorized" });
   }
