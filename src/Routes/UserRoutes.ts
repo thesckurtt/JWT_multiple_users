d8587ad38713778a04aa883e1b63ebb1f0267103
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2024-12-27 11:17:19 -0300

    Update UserRoutes.ts

--- src/Routes/UserRoutes.ts
+++ src/Routes/UserRoutes.ts
@@ -1,5 +1,6 @@
 import { UserController } from "../Controller/UserController/UserController.js";
 import { FastifyInstance } from "fastify";
 export async function userRoutes(fastify: FastifyInstance) {
+  fastify.addHook("onRequest", fastify.UserAuthenticate);
   fastify.get("/", UserController.home);  
 }
