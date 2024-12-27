253f95df773bd4643e809362af48bbe519492adf
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2024-12-27 16:46:15 -0300

    Update auth.test.ts

--- src/tests/integration/auth.test.ts
+++ src/tests/integration/auth.test.ts
@@ -1,17 +1,31 @@
-import {app} from "../../server.js"
+import { app } from "../../server.js";
+import request from "supertest";
 
-describe("Auth Routes", ()=>{
-  beforeAll(async ()=>{
+describe("Auth Routes", () => {
+  beforeAll(async () => {
     await app.ready(); // Ensures that the server is ready
-  })
+  });
 
-  afterAll(async ()=>{
-    app.close() // Ensures that the server is close 
-  })
+  afterAll(async () => {
+    app.close(); // Ensures that the server is close
+  });
 
-  describe("Route /auth/login", ()=>{
-    it("should return a token for valid credentials", ()=>{
-      expect(true).toBe(true);
-    })
-  })
+  describe("Route /auth/login", () => {
+    it("should return a token for valid credentials", async () => {
+      const credentials = {
+        email: "Eden_Nienow@hotmail.com",
+        password: "admin",
+      };
+
+      const response = await request(app.server).post("/auth/login").send(credentials);
+
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty("token");
+      expect(typeof response.body.token).toBe("string");
+      
+      // expect(response.body).toHaveProperty("token");
+
+      // expect(true).toBe(true);
+    });
+  });
 });
