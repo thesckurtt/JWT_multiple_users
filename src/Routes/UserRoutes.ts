ea3f9bf3b8915e49d296f05bd058130fb4bee30e
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2024-12-27 09:11:46 -0300

    Update UserRoutes.ts

---
+++ src/Routes/UserRoutes.ts
@@ -0,0 +1,5 @@
+import { UserController } from "../Controller/UserController/UserController.js";
+import { FastifyInstance } from "fastify";
+export async function userRoutes(fastify: FastifyInstance) {
+
+}
