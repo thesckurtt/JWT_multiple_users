f707a6f291d5870f675ff6aa8f521491b9032276
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2024-12-26 23:12:08 -0300

    Update AdminRoutes.ts

--- src/Routes/AdminRoutes.ts
+++ src/Routes/AdminRoutes.ts
@@ -1,3 +1,4 @@
+import { AdminController } from '../Controller/AdminController/AdminController.js';
 import { FastifyInstance,FastifyRequest, FastifyReply } from 'fastify';
 
 export async function AdminRoutes(fastify: FastifyInstance) {
@@ -5,4 +6,5 @@ export async function AdminRoutes(fastify: FastifyInstance) {
   fastify.get("/", async (request: FastifyRequest, reply: FastifyReply)=>{
     return "Hello Admin!"
   })
+  fastify.post("/black-list/add", AdminController.BlackList_add)
 }
